36f92cc905937d2f13dd3299d779fdac10e4b7c9
Jestro Diaz <jestrodiaz006@gmail.com>
2025-01-16 02:20:43 +0800

    add authentication context and implement login/signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //pages & components
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -12,10 +14,18 @@ function App() {
         <div className='pages'>
           <Routes>
             <Route 
-
             path="/"
             element={<Home/>}
+            />
 
+            <Route
+            path="/login"
+            element={<Login/>}
+            />
+
+            <Route 
+            path="/signup"
+            element={<Signup/>}
             />
           </Routes>
         </div>
