4f60bb647d16764ec2c8b2530e96b5170afd85cd
Jestro Diaz <jestrodiaz006@gmail.com>
2025-01-15 10:52:24 +0800

    add workout context and CRUD functionality for workouts

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -2,29 +2,35 @@ import { createContext, useReducer } from 'react'
 
 export const WorkoutsContext = createContext()
 
-export const workoutReducer = (state, action) => {
+export const workoutsReducer = (state, action) => {
     switch (action.type) {
         case 'SET_WORKOUTS':
             return {
                 workouts: action.payload
             }
+
         case 'CREATE_WORKOUT':
             return {
                 workouts: [action.payload, ...state.workouts]
             }
-            default:
+
+        case 'DELETE_WORKOUT':
+            return {
+                workouts: state.workouts.filter((w) => w._id !== action.payload._id )
+            }
+        default:
                 return state
     }
 }
 
 export const WorkoutsContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(workoutReducer,{
+    const [state, dispatch] = useReducer(workoutsReducer,{
         workouts: null
     })
     
 
     return (
-        <WorkoutsContext.Provider value={{state, dispatch}}>
+        <WorkoutsContext.Provider value={{...state, dispatch}}>
             { children }
         </WorkoutsContext.Provider>
     )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <WorkoutsContextProvider>
-    <App />
+      <App />
     </WorkoutsContextProvider>
   </React.StrictMode>
 );
