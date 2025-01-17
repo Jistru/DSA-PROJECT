cd49ceb4ff1e770e2bca4f09fe6699a699f94938
Jestro Diaz <jestrodiaz006@gmail.com>
2025-01-18 01:32:35 +0800

    add authentication context and hooks for user state management

--- frontend/src/hooks/useWorkoutsContext.js
+++ frontend/src/hooks/useWorkoutsContext.js
@@ -4,7 +4,7 @@ import { useContext } from 'react';
 export const useWorkoutsContext = () => {
     const context = useContext( WorkoutsContext )
 
-    if(!context) {
+    if (!context) {
         throw Error('useWorkoutsContext must be used inside an WorkoutContextProvider')
     }
 
